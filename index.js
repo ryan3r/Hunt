f57a48a9c3ce34b6923c1910730d3a490cab8264
Ryan Ray <ryan314r@gmail.com>
2018-12-19 02:38:01 -0600

    Clean up unused vars and indentation errors

--- index.js
+++ index.js
@@ -10,13 +10,6 @@ const MINUTE_IN_MS = 60000;
 const DAY_IN_MINUTES = 1440;
 const HOUR_IN_MINUTES = 60;
 
-const CODE_CHARS = [
-	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
-	"Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f",
-	"g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
-	"w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
-];
-
 var data = require("./data");
 
 // save the data
